a647d579ed59bcdce022fa604a8748d0cd68105f
Shreyansh Daga <sdaga@usc.edu>
2014-11-03 04:10:29 -0800

    - Pushing Greedy class

---
+++ Algorithms/GreedyAlgorithms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class GreedyAlgorithms
+    {
+        // This class will deal with greedy algorithms
+
+    }
+}
